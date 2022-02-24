91c6b0a9cab35388feaa831b763107967adc89b1
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-02-24 12:40:41 +0100

    added reservation model validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,23 @@
 class Reservation < ApplicationRecord
+  require 'date'
   belongs_to :user
   belongs_to :car
+
+
+  validates :status, inclusion: {in: statuses}
+  validates :checkOutTime, presence: true
+  validates :expectedReturnTime, presence: true
+
+
+  validate :orderTimeValidation
+
+  def orderTimeValidation
+    if checkOutTime < Time.now - 5.minutes || checkOutTime > Time.now + 7.days
+      errors.add(:checkOutTime, "should within 7 days from now!")
+    elsif expectedReturnTime < Time.now || expectedReturnTime > Time.now + 7.days
+      errors.add(:expectedReturnTime, "should within 7 days from now!")
+      elsif (expectedReturnTime - checkOutTime ) < 1.hour || (expectedReturnTime - checkOutTime) > 10.hours
+        errors.add(:expectedReturnTime, "should be within 1-10 hours of checkout time!")
+    end
+  end
 end
